797db31023da90f1d68dc2495bcfbb5d5c49fe03
Taiwo Ogbonyomi <taiwoogbonyomi@gmail.com>
2024-05-05 17:12:04 +0000

    Add function to create two random numbers between 1 and 25

---
+++ assets/js/script.js
@@ -0,0 +1,56 @@
+//wait for the dom to finish loading before running the game
+//Get the buttons elements and add event listeners to them
+
+
+document.addEventListener ("DOMContentLoaded", function() {
+    let buttons = document.getElementsByTagName ("button");
+
+    for (let button of buttons){
+        button.addEventListener("click", function(){
+            if (this.getAttribute("data-type") === "submit") {
+                alert("You clicked submit!");
+            } else{
+                let gameType =this.getAttribute("data-type");
+                alert(`You clicked $(gameType)`);
+            }
+        })
+
+        
+    }
+})
+
+function runGame() {
+    //create two random numbers between 1 and 25
+    let num1 = Math.floor(Math.random() * 25 ) + 1;
+    let num2 = Math.floor(Math.random() * 25 ) + 1;
+    
+
+}
+
+function checkAnswer() {
+
+}
+
+function checkCorrectAnswer() {
+
+}
+
+function incrementScore() {
+
+}
+
+function incrementWrongAnswer() {
+
+}
+
+function displayAdditionQuestion() {
+
+}
+
+function displaySubtractQuestion() {
+
+}
+
+function displayMultiplyQuestion() {
+
+}
